81fcc9afb111a452471a29ebc35dad05e4eb96e2
vjk7989 <92521801+vjk7989@users.noreply.github.com>
2024-08-26 03:17:54 +0530

    Update script.js

--- script.js
+++ script.js
@@ -24,7 +24,7 @@ const nextBtn = $.getElementById("next")
 const background = $.getElementById("background")
 
 const songs = [
-	{path:"Lost in Your Eyes.wav",musicName:"Lost in Your Eyes",artist:"Krishna Tadepalli",cover:"Designer (3).png"},
+	{path:"Lost in Your Eyes.wav",musicName:"THE Ojazos",artist:"Krishna Tadepalli",cover:"Designer (3).png"},
 	{path:"Always Here.mp3",musicName:"Always Here",artist:"Vijay Tadepalli",cover:"Designer (4).png"},
 	{path:"By your side.mp3",musicName:"By your side",artist:"VIJAYA KRISHNA TADEPALLI",cover:"Designer (6).png"}
     
